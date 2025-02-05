94ebf541c6d039080e881bea60dd18193ed353fa
AaqibWiki <aaqibgahamed@gmail.com>
2025-02-05 17:22:18 +0530

    Update Node.cs

--- Short_Route_Optimizer_API/Entities/Node.cs
+++ Short_Route_Optimizer_API/Entities/Node.cs
@@ -3,5 +3,5 @@ namespace Short_Route_Optimizer_API.Entities;
 public class Node
 {
     public string? Name { get; set; }
-    public Dictionary<Node, int> Neighbors { get; set; } = new Dictionary<Node, int>();
+    public Dictionary<string, int> Neighbors { get; set; } = new Dictionary<string, int>();
 }
